229596b5420548fa423a5c2befba3a99414daecb
Siegfried Vanderloock <svanderloock@outlook.com>
2026-03-24 11:47:34 +0100

    Adds comparison of ROW_NUMBER, RANK, and DENSE_RANK
    Enhances the query to demonstrate and compare the results of three different window functions for selecting the top 3 most expensive tracks per album.
    Provides clearer insight into how each ranking method handles ties and ordering.

--- scripts/window_functions/top_3_most_expensive_tracks_per_album.sql
+++ scripts/window_functions/top_3_most_expensive_tracks_per_album.sql
@@ -1,13 +1,24 @@
--- This query retrieves the top 3 most expensive tracks for each album in the Chinook database.
--- In case of ties in UnitPrice, it orders by TrackId to ensure consistent results.
-with cte_ranked_tracks as (
+with cte_tracks_rownumber as (
     select 
         tracks.TrackId, 
         albums.AlbumId, 
         tracks.UnitPrice,
-        ROW_NUMBER() OVER (PARTITION BY albums.AlbumId ORDER BY tracks.UnitPrice DESC, tracks.TrackId ASC) as row_num
-    from chinook_dev.albums albums
-    join chinook_dev.tracks tracks on albums.AlbumId = tracks.AlbumId
-)
-select  * from cte_ranked_tracks
+        -- In case of ties in UnitPrice, we order by TrackId to ensure consistent results.
+        ROW_NUMBER() OVER (PARTITION BY tracks.AlbumId ORDER BY tracks.UnitPrice DESC, tracks.TrackId ASC) as row_num,
+        RANK() OVER (PARTITION BY tracks.AlbumId ORDER BY tracks.UnitPrice DESC, tracks.TrackId ASC) as rank_num,
+        DENSE_RANK() OVER (PARTITION BY tracks.AlbumId ORDER BY tracks.UnitPrice DESC, tracks.TrackId ASC) as dense_rank_num
+    from chinook_dev.tracks tracks
+    join chinook_dev.albums albums on albums.AlbumId = tracks.AlbumId
+),
+cte_result as (
+select  TrackId, AlbumId, UnitPrice, row_num as num, 'ROW_NUMBER' as window_function from cte_tracks_rownumber
 where row_num <= 3
+UNION
+select  TrackId, AlbumId, UnitPrice, rank_num as num, 'RANK' as window_function from cte_tracks_rownumber
+where rank_num <= 3
+UNION
+select  TrackId, AlbumId, UnitPrice, dense_rank_num as num, 'DENSE_RANK' as window_function from cte_tracks_rownumber
+where dense_rank_num <= 3
+)
+select AlbumId, window_function, TrackId, UnitPrice, num from cte_result
+order by AlbumId, window_function, TrackId, UnitPrice, num ASC;
